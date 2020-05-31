fc04f326964960c77dcbc442d7a749c1bf1771e1
Seweryn Kras <sewerynkras@gmail.com>
2020-05-31 18:21:40 +0200

    init tests

---
+++ like_object/tests/conftest.py
@@ -0,0 +1,85 @@
+import pytest
+from django.contrib.auth.models import User
+from follow_object.views import FollowObjectViewSet
+from follow_object.models import FollowObject
+from user_profile.models import Profile
+
+pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture
+def testUser0() -> Profile:
+    """
+    Generate a user
+    """
+    user = User.objects.create_user(
+        username="testUser",
+        email="admin@email.com",
+        password="testPassword123",
+        id=1)
+    profile = Profile.objects.create(
+        user=user,
+        display_name="testUser name",
+        id=1
+    )
+    return profile
+
+
+@pytest.fixture
+def testUser1() -> Profile:
+    """
+    Generate a user
+    """
+    user = User.objects.create_user(
+        username="testUser1",
+        email="admin@email123.com",
+        password="testPassword123",
+        id=2)
+    profile = Profile.objects.create(
+        user=user,
+        display_name="testUser1 name",
+        id=2)
+
+    return profile
+
+
+@pytest.fixture
+def APIClient_no_auth():
+    """
+    Generate a rest_frameworks APICLient and authenticates it
+    with testUser
+    """
+    from rest_framework.test import APIClient
+    client = APIClient()
+    return client
+
+
+@pytest.fixture
+def APIClient(testUser0):
+    """
+    Generate a rest_frameworks APICLient and authenticates it
+    with testUser
+    """
+    from rest_framework.test import APIClient
+    client = APIClient()
+    client.force_authenticate(testUser0)
+    return client
+
+
+@pytest.fixture
+def testViewTest(testUser0: Profile, testUser1: Profile) -> LikeObjectViewSet:
+    """
+    Creates 3 Follows and returns a
+    LikeObjectViewSet instance
+    """
+    tweet1 = TweetItem.objects.create(
+        text="Test tweet 1!", author=testUser0, id="1")
+    tweet2 = TweetItem.objects.create(
+        text="Test tweet 2!", author=testUser1, id="2")
+    like1 = LikeObject.object.create(
+        tweet=tweet1, author=testUser1
+    )
+    like2 = LikeObject.object.create(
+        tweet=tweet2, author=testUser0
+    )
+    return LikeObjectViewSet()
---
+++ like_object/tests/test_like_object.py
@@ -0,0 +1,68 @@
+from django.urls import reverse
+import pytest
+from user_profile.models import Profile
+from user_profile.views import ProfileViewSet
+from django.contrib.auth.models import User
+
+pytestmark = pytest.mark.django_db
+
+
+def test_00_correct_get(APIClient, testViewTest):
+    response = APIClient.get("/tweets/1/likes/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_01_correct_get_no_auth(APIClient_no_auth, testViewTest):
+    response = APIClient_no_auth.get("/tweets/1/likes/")
+    assert response.status_code == 200
+    assert len(response.json()) == 1
+
+
+def test_02_correct_post(APIClient, testViewTest):
+    data = {"id": 1}
+    response = APIClient.post("/likes/", data)
+    assert response.status_code == 201
+
+    # Check that it got created
+    response = APIClient.get("/tweets/1/likes/")
+    assert response.status_code == 200
+    assert len(response.json()) == 2
+
+
+def test_03_incorrect_post_no_auth(APIClient_no_auth, testViewTest):
+    data = {"id": 1}
+    response = APIClient_no_auth.post("/likes/", data)
+    assert response.status_code == 403
+
+
+def test_04_incorrect_post_duplicate(APIClient, testViewTest):
+    data = {"id": 2}
+    response = APIClient.post("/likes/", data)
+    assert response.status_code == 400
+
+
+def test_05_incorrect_post_not_found(APIClient, testViewTest):
+    data = {"id": 1234}
+    response = APIClient.post("/likes/", data)
+    assert response.status_code == 400
+
+
+def test_06_correct_delete(APIClient, testViewTest):
+    response = APIClient.delete("/likes/2")
+    assert response.status_code == 204
+
+    # Check that it got deleted
+    response = APIClient.get("/tweets/2/likes/")
+    assert response.status_code == 200
+    assert len(response.json()) == 0
+
+
+def test_07_incorrect_delete_like_not_found(APIClient, testViewTest):
+    response = APIClient.delete("/likes/1")
+    assert response.status_code == 404
+
+
+def test_08_incorrect_delete_tweet_not_found(APIClient, testViewTest):
+    response = APIClient.delete("/likes/1234")
+    assert response.status_code == 404
